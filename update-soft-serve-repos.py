b912dd027a175ff5d58f06910148d95804a7eb4b
Daylin Morgan <daylinmorgan@gmail.com>
2022-08-14 23:40:37 +0000

    add pruning step for old repos

--- update-soft-serve-repos.py
+++ update-soft-serve-repos.py
@@ -6,23 +6,30 @@ import shutil
 GITEA_REPOS = Path(__file__).parent / "gitea/git/repositories/daylin"
 SOFT_REPOS = Path(__file__).parent / "soft/repos"
 
-allowed = ["dotfiles", "gitea-server"]
+allowed = ["dotfiles", "git-server"]
+soft_serve_only = ["config"]
 
 
 def main():
 
     for repo in GITEA_REPOS.iterdir():
         name = repo.name.replace(".git", "")
-        if name not in allowed:
-            continue
-        print(repo)
-
         dest = SOFT_REPOS / name
-        print(f">> {dest}")
+        if name not in [*allowed, *soft_serve_only]:
+            continue
+
+        print(f"{repo} >> {dest}")
 
         if dest.is_dir():
             shutil.rmtree(dest)
         shutil.copytree(repo, dest)
+
+    for repo in SOFT_REPOS.iterdir():
+        name = repo.name.replace(".git", "")
+        dest = SOFT_REPOS / name
+        if name not in [*allowed, *soft_serve_only]:
+            print(f"pruning {name}")
+            shutil.rmtree(repo)
 
 
 if __name__ == "__main__":
